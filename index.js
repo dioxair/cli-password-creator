5f5b378573a4dd9cf43ad2885e24e9b820699cde
Samuel Olagunju <dioxair@gmail.com>
2022-06-09 12:06:08 -0400

    Created function for generating passwords

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ console.log("How long should the password be?");
 
 let passwordLength = prompt(">> ");
 passwordLength = Number(passwordLength);
+
+function getPassword() {
+  let values =
+    "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+  let password = "";
+  for (let i = 0; i <= passwordLength; i++) {
+    password += values.charAt(
+      Math.floor(Math.random() * Math.floor(values.length - 1))
+    );
+  }
+
+  return password;
+}
