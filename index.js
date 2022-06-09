57be8ef30d517a564bab67421a810f447efc4ece
Samuel Olagunju <dioxair@gmail.com>
2022-06-09 12:00:26 -0400

    Added prompt for password length

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const prompt = require("prompt-sync")({ sigint: true });
+
+console.log("How long should the password be?");
+
+let passwordLength = prompt(">> ");
+passwordLength = Number(passwordLength);
